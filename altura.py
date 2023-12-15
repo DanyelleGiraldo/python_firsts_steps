2e38f95e62b8383d5b4a5f12622888427bab9962
DanyelleGiraldo <danyellesgiraldoj@gmail.com>
2023-12-15 17:20:42 -0500

    nuevos archivos: altura.py

---
+++ altura.py
@@ -0,0 +1,7 @@
+#Escriba un programa que dibuje el triángulo del tamaño indicado por el usuario de acuerdo al ejemplo
+altura= int(input("Altura: "))
+
+for i in range(altura):       
+      for x in range(i+1):
+          print("*", end=" " )
+      print(" ")  
