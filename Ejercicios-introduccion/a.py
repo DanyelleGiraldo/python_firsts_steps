a23ff201838c299317fb7f70545fcc2184229cc3
DanyelleGiraldo <danyellesgiraldoj@gmail.com>
2023-12-21 14:12:43 -0500

    nuevos archivos: Ejercicios-introduccion/a.py

---
+++ Ejercicios-introduccion/a.py
@@ -0,0 +1,5 @@
+# ⚙ Desarrolle un programa que tenga la siguiente entrada:
+# - primero, el usuario ingresa un número entero *n*, que indica cuántas palabras ingresará a continuación;
+# - después el usuario ingresa *n* palabras.
+# La salida del programa debe mostrar la palabra más larga y la más corta que fueron ingresadas por el usuario.
+
