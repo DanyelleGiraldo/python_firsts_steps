8b78f4c1283bd4432a08d8e297058eb142d8f310
DanyelleGiraldo <danyellesgiraldoj@gmail.com>
2024-01-03 16:42:55 -0500

    nuevos archivos: taller/__pycache__/utils.cpython-310.pyc 	nuevos archivos: taller/main.py 	modificados:     taller/utils.py

--- taller/utils.py
+++ taller/utils.py
@@ -1,5 +1,4 @@
 def ingresardatonumero(enunciado):
-    dato=input(enunciado)
     
     while (True):
         dato=input(enunciado)
@@ -9,6 +8,38 @@ def ingresardatonumero(enunciado):
             break
         else: 
             print("no es entero")
-            break
             
-    
+            
+def ingresadatocadena(dato):
+    while (True):
+        cadena= input(dato)
+        if cadena.isalpha():
+            print("la cadena no tiene numeros")
+            return str(cadena)
+            break
+        else:
+            print("la cadena tiene digitos")
+            
+def ingresardatopositivo(dato):
+    while True:
+        input_str = input(dato)
+        if input_str.isdigit():
+            numero = int(input_str)
+            if numero > 0:
+                print("El número es positivo")
+                return numero
+            else:
+                print("El número no es positivo")
+        else:
+            print("No ingresó un número entero")
+        
+def ingresardatonumeroentre(dato):
+    while (True):
+        numero=int(input(dato))
+        if numero >= 0 and numero<= 10:
+            print("el numero esta dentro del rango:D")
+            return int(numero)
+            break
+        else:
+            print("el numero esta fuera del rango")
+            
